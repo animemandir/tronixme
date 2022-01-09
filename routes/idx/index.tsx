d57f5c13cc2d83ae1845a6afc2c1dde6aa1aeada
Donatas Bucinskas <donatas@tronikel.com>
2022-01-09 19:02:14 +0200

    frame for index page now looks better

--- routes/idx/index.tsx
+++ routes/idx/index.tsx
@@ -1,4 +1,16 @@
-import { Box, Card, CardContent, Container, Grid, Paper, Typography } from "@mui/material";
+import {
+    Box,
+    Card,
+    CardActionArea,
+    CardContent,
+    CardMedia,
+    Container,
+    Grid,
+    Paper,
+    Stack,
+    Typography,
+} from "@mui/material";
+import Image from "next/image";
 import useSWR from "swr";
 
 import { APIRecently } from "@types";
@@ -8,14 +20,29 @@ export default function Index() {
 
     return (
         <Container sx={{ my: 6 }}>
-            <Paper component={Box} width="100%">
+            <Paper variant="outlined" component={Stack} width="100%" p={2}>
+                <Typography variant="h4" align="left" gutterBottom>
+                    Recently updated
+                </Typography>
                 <Grid width="100%" container spacing={2}>
                     {data.map(({ title, latest_ep, thumbnail }) => (
-                        <Grid item xs={6} md={4} key={`${title}${latest_ep}`}>
+                        <Grid item xs={12} md={3} key={`${title}${latest_ep}`}>
                             <Card sx={{ width: "100%" }}>
-                                <CardContent>
-                                    <Typography>{title}</Typography>
-                                </CardContent>
+                                <CardActionArea>
+                                    <CardMedia sx={{ height: 400 }}>
+                                        <Box position="relative" width="100%" height="100%">
+                                            <Image
+                                                src={thumbnail}
+                                                layout="fill"
+                                                objectFit="cover" // or objectFit="cover"
+                                            />
+                                        </Box>
+                                    </CardMedia>
+
+                                    <CardContent>
+                                        <Typography>{title}</Typography>
+                                    </CardContent>
+                                </CardActionArea>
                             </Card>
                         </Grid>
                     ))}
