8ebfa3f0642edb43eac8279fa33f65ca5212a91b
Donatas Bucinskas <donatas@tronikel.com>
2022-01-09 19:19:27 +0200

    add links

--- routes/idx/index.tsx
+++ routes/idx/index.tsx
@@ -12,9 +12,46 @@ import {
     Typography,
 } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
 import useSWR from "swr";
 
 import { APIRecently } from "@types";
+
+interface AnimeCardProps {
+    title: string;
+    ep: string;
+    thumbnail: string;
+    url: string;
+}
+
+const AnimeCard = ({ ep, thumbnail, title, url }: AnimeCardProps) => {
+    return (
+        <Card sx={{ width: "100%" }}>
+            <Link passHref href={`${url}/${ep}`}>
+                <CardActionArea LinkComponent="a">
+                    <CardMedia sx={{ height: 400 }}>
+                        <Box position="relative" width="100%" height="100%">
+                            <Image
+                                src={thumbnail}
+                                layout="fill"
+                                objectFit="cover" // or objectFit="cover"
+                            />
+                        </Box>
+                    </CardMedia>
+
+                    <CardContent>
+                        <Typography gutterBottom variant="h6">
+                            {title}
+                        </Typography>
+                        <Typography variant="body1" color="text.secondary">
+                            {`Ep. ${ep}`}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Link>
+        </Card>
+    );
+};
 
 export default function Index() {
     const { data = [] } = useSWR<APIRecently[]>("/recently/sub");
@@ -35,30 +72,14 @@ export default function Index() {
                 </Stack>
 
                 <Grid container spacing={4}>
-                    {data.map(({ title, latest_ep, thumbnail }) => (
+                    {data.map(({ title, latest_ep, thumbnail, url }) => (
                         <Grid item xs={12} sm={6} md={4} lg={3} key={`${title}${latest_ep}`}>
-                            <Card sx={{ width: "100%" }}>
-                                <CardActionArea>
-                                    <CardMedia sx={{ height: 400 }}>
-                                        <Box position="relative" width="100%" height="100%">
-                                            <Image
-                                                src={thumbnail}
-                                                layout="fill"
-                                                objectFit="cover" // or objectFit="cover"
-                                            />
-                                        </Box>
-                                    </CardMedia>
-
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h6">
-                                            {title}
-                                        </Typography>
-                                        <Typography variant="body1" color="text.secondary">
-                                            {`Ep. ${latest_ep}`}
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
+                            <AnimeCard
+                                url={url}
+                                ep={latest_ep}
+                                thumbnail={thumbnail}
+                                title={title}
+                            />
                         </Grid>
                     ))}
                 </Grid>
