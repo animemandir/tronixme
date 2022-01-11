ebc2e6f1c6d5e468d7d403d068370e8be4c431ed
Donatas Bucinskas <donatas@tronikel.com>
2022-01-11 19:54:18 +0200

    add ssr to search page

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,5 +1,35 @@
+import { GetServerSideProps } from "next";
+import Error from "next/error";
+import { SWRConfig } from "swr";
+
+import { PageProps } from "@types";
+
+import { axiosSSR, handleSSR } from "@utils";
+
 import Search from "@routes/search";
 
-export default function Page() {
-    return <Search />;
+export const getServerSideProps: GetServerSideProps = ({ req, query }) =>
+    handleSSR(async () => {
+        const { host } = req.headers;
+        const { q } = query;
+
+        const { data: anime } = await axiosSSR(`/anime/search?q=${q}`, String(host));
+
+        const fallback = {
+            [`/anime/search?q=${q}`]: anime,
+        };
+
+        return { props: { fallback } };
+    });
+
+export default function Page({ fallback, error }: PageProps) {
+    if (error) {
+        return <Error title={error.data} statusCode={error.status as any} />;
+    }
+
+    return (
+        <SWRConfig value={{ fallback }}>
+            <Search />
+        </SWRConfig>
+    );
 }
