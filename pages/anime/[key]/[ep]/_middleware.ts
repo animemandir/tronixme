5fea6b35fb901aa33001d3799f4eb783d0d97d5d
Donatas Bucinskas <donatas@tronikel.com>
2022-01-18 21:34:00 +0200

    bump

--- pages/anime/[key]/[ep]/_middleware.ts
+++ pages/anime/[key]/[ep]/_middleware.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
     const { secret } = req.cookies;
+
     if (secret !== process.env.SECRET) {
         return NextResponse.redirect("/auth");
     }
