d61cdee413e69b405245efbfd6c62b7c43e3865e
Donatas Bucinskas <donatas@tronikel.com>
2022-01-18 20:49:50 +0200

    add middleware that locks you out of an episode wout secret

---
+++ pages/anime/[key]/[ep]/_middleware.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+    const { secret } = req.cookies;
+    if (secret !== process.env.SECRET) {
+        return NextResponse.redirect("/");
+    }
+}
