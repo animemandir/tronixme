334595b70dd7809d6a96e69b1df6ef1e9d1b0da7
Donatas Bucinskas <donatas@tronikel.com>
2022-01-20 15:44:19 +0200

    change casing

---
+++ tronixme/components/Footer.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+
+export function Footer() {
+    return (
+        <footer>
+            <Typography gutterBottom align="center" variant="body2" my={2}>
+                Disclaimer: This site does not store any files on its server. All contents are
+                provided by non-affiliated third parties.
+            </Typography>
+        </footer>
+    );
+}
