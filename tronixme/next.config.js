6ef084d6267dde29d785c6b7a887c8740f117c63
Donatas Bucinskas <donatas@tronikel.com>
2022-01-20 17:23:25 +0200

    rm config thingy

--- tronixme/next.config.js
+++ tronixme/next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    experimental: {
-        externalDir: true,
-    },
     reactStrictMode: true,
     images: {
         domains: ["animedao.to"],
-    },
+    },  
 };
