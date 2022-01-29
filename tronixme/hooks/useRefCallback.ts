aad4a19a724d1664833b975f75d73e5b01837a94
Donatas Bucinskas <donatas@tronikel.com>
2022-01-29 21:21:55 +0200

    forgot deps

--- tronixme/hooks/useRefCallback.ts
+++ tronixme/hooks/useRefCallback.ts
@@ -4,13 +4,15 @@ import { useCallback, useRef } from "react";
 export function useRefCallback<T>(callback: (el: T) => void, cleanup: () => void = () => {}) {
     const ref = useRef<T | null>(null);
 
-    const refCallback = useCallback(node => {
-        if (ref.current) cleanup();
-        if (node) callback(node as any as T);
+    const refCallback = useCallback(
+        node => {
+            if (ref.current) cleanup();
+            if (node) callback(node as any as T);
 
-        ref.current = node;
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+            ref.current = node;
+        },
+        [callback, cleanup]
+    );
 
     return [refCallback, ref] as [typeof refCallback, typeof ref];
 }
